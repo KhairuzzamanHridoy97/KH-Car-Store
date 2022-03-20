a0180e62dd806e4e9663835967d560c108ca7a9d
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-03-20 23:00:41 +0600

    fisrt commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from './pages/Home/Home/Home';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
+
+  
+
+     
+    </div>
+  );
+}
+
+export default App;
