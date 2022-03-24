43754be535dbf2b51610dfa58204d21a9cacfe67
KhairuzzamanHridoy97 <khairuzzaman15-1273@diu.edu.bd>
2022-03-24 16:10:50 +0600

    set deleteOrder in myOrder

--- src/pages/Dashboard/MyOrder/MyOrder.js
+++ src/pages/Dashboard/MyOrder/MyOrder.js
@@ -13,14 +13,6 @@ const MyOrder =()=>{
     const {user}=  useAuth();
     const [ myOrders,setMyOrders]= useState([]);
 
-    const [product,setProduct]= useState([])
-    useEffect(()=>{
-        fetch('http://localhost:5000/products')
-        .then(res=>res.json())
-        .then(data=>setProduct(data))
-        
-    },[])
-
 
     useEffect(()=>{
         const url=`http://localhost:5000/orders?email=${user.email}`
@@ -28,6 +20,27 @@ const MyOrder =()=>{
         .then(res=>res.json())
         .then(data=>setMyOrders(data))
     },[]);
+
+    //delete order
+    const handleDelete = (id) => {
+        console.log(id);
+        const proceed = window.confirm("Are you sure, you want to delete?");
+        if (proceed) {
+            fetch(`http://localhost:5000/deleteOrder/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert("Deleted Successfully");
+                        const remainingOrders = myOrders?.filter(myOrder => myOrder._id !== id);
+                        setMyOrders(remainingOrders);
+                    }
+                });
+            console.log(id);
+        }
+    };
     
     return (
         <div>
@@ -40,6 +53,7 @@ const MyOrder =()=>{
             <TableCell>Name</TableCell>
             <TableCell align="right">Email</TableCell>
             <TableCell align="right">Location</TableCell>
+            <TableCell align="right">Date</TableCell>
             <TableCell align="right">Car Name</TableCell>
             <TableCell align="right">Price</TableCell>
             <TableCell align="right">Action</TableCell>
@@ -55,12 +69,13 @@ const MyOrder =()=>{
               <TableCell component="th" scope="row">
                 {myOrder.name}
               </TableCell>
-              <TableCell align="right">{user.email}</TableCell>
+              <TableCell align="right">{myOrder.email}</TableCell>
               <TableCell align="right">{myOrder.location}</TableCell>
+              <TableCell align="right">{myOrder.date}</TableCell>
               <TableCell align="right">{myOrder.title}</TableCell>
               <TableCell align="right">{myOrder.price}</TableCell>
               <TableCell align="right">
-                  <button className="btn btn-danger">Cancel</button>
+                  <button onClick={() => handleDelete(myOrder?._id)} className="btn btn-danger">Cancel</button>
               </TableCell>
               {/* <TableCell align="right">{row.carbs}</TableCell> */}
               {/* <TableCell align="right">{row.protein}</TableCell> */}
